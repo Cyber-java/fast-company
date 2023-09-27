be3a65d28f54170921093f979962d6e4518b9fb6
Cyber-java <saalux@yandex.ru>
2023-09-27 17:28:08 +0300

    add render Pharse

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,12 +4,31 @@ import API from "../api";
 const Users = () => {
   const [users, setUsers] = useState(API.users.fetchAll());
   const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== userId));
+    setUsers(users.filter((user) => user._id !== userId));
   };
-  const handlePharse = (number) => {};
+  const renderPharse = (number) => {
+    const lastone = Number(number.toString().slice(-1));
+    console.log(lastone);
+    console.log(number.toString());
+    if (number > 4 && number < 15) return "человек тусанет";
+    if ([2, 3, 4].indexOf(lastone) >= 0) return "человека тусанут";
+    if (lastone === 1) return "человек туснет";
+    return "человек тусанет";
+  };
 
   return (
     <>
+      <h2>
+        <span
+          className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+        >
+          {users.length > 0
+            ? `${
+                users.length + " " + renderPharse(users.length)
+              } c тобой сегодня`
+            : "Никто не тусанет с тобой сегодня"}
+        </span>
+      </h2>
       {users.length > 0 && (
         <table className="table">
           <thead>
@@ -19,28 +38,36 @@ const Users = () => {
               <th scope="col">Профессия</th>
               <th scope="col">Встретился,раз</th>
               <th scope="col">Рейтинг</th>
+              <th />
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => {
-              return (
-                <tr key={user._id}>
-                  <td key={user._id}>{user.name}</td>
-                  <td>{user.qualities.map((qualitie) => qualitie.name)}</td>
-                  <td>{user.profession.name}</td>
-                  <td>{user.completedMeetings}</td>
-                  <td>{user.rate}</td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleDelete(user._id)}
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((qualitie) => (
+                    <span
+                      className={"badge m-1 bg-" + qualitie.color}
+                      key={qualitie._id}
                     >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
+                      {qualitie.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate}/5</td>
+                <td>
+                  <button
+                    className="btn btn-danger m-2"
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       )}
