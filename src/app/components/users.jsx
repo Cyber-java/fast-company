c86b6a5f5a426493555e36b6cc1a3255b4f4e273
Cyber-java <saalux@yandex.ru>
2023-10-03 19:31:34 +0300

    create GroupeList add PorpTypes in BookMark create Professionals.api

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,48 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import User from "./user";
 import Pagination from "./pagination";
 import { paginate } from "../utils/paginate";
+import GroupList from "./groupList";
+import PropTypes from "prop-types";
+import api from "../api";
 
 const Users = ({ users, ...rest }) => {
-  const count = users.length;
-  const pageSize = 4;
-  const [currentPage, setCurrentPage] = useState(1);
-  const handlePageChange = (pageIndex) => {
-    setCurrentPage(pageIndex);
-  };
+    const count = users.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions] = useState(api.professions.fetchAll());
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+    const handleItemsSelect = (params) => {
+        console.log(params);
+    };
+    console.log(professions);
+    const userCrop = paginate(users, currentPage, pageSize);
 
-  const userCrop = paginate(users, currentPage, pageSize);
-
-  return (
-    <>
-      {count > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился,раз</th>
-              <th scope="col">Рейтинг</th>
-              <th scope="col">Избранное</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {userCrop.map((user) => (
-              <User key={user._id} {...rest} {...user} />
-            ))}
-          </tbody>
-        </table>
-      )}
-      <Pagination
-        itemsCount={count}
-        pageSize={pageSize}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-    </>
-  );
+    return (
+        <>
+            <GroupList
+                items={professions}
+                onItemsSelected={handleItemsSelect}
+            />
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился,раз</th>
+                            <th scope="col">Рейтинг</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userCrop.map((user) => (
+                            <User key={user._id} {...rest} {...user} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
 };
-
+Users.propTypes = {
+    users: PropTypes.array.isRequired
+};
 export default Users;
